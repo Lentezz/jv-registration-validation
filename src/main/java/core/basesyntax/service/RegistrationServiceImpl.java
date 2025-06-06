ecbc2fb3a58c53641681985184be946172b3fb2b
patronus246 <pespatron.original@gmail.com>
2025-06-06 23:02:46 +0300

    Implement RegistrationService and cover by tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,37 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidUserDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_LOGIN_LENGTH = 6;
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws InvalidUserDataException {
+        if (user == null) {
+            throw new InvalidUserDataException("User has invalid details.");
+        }
+        if (!validatePassword(user.getPassword())) {
+            throw new InvalidUserDataException("Invalid password.");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new InvalidUserDataException("User is too young.");
+        }
+        if (!validateLogin(user.getLogin())) {
+            throw new InvalidUserDataException("Invalid login or login already exists.");
+        }
+        return storageDao.add(user);
+    }
+
+    private boolean validatePassword(String password) {
+        return password != null && password.length() >= MIN_PASSWORD_LENGTH;
+    }
+
+    private boolean validateLogin(String login) {
+        return login != null && storageDao.get(login) == null && login.length() >= MIN_LOGIN_LENGTH;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,119 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.InvalidUserDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+        user.setAge(18);
+        user.setLogin("lentez");
+        user.setPassword("password");
+    }
+
+    @Test
+    void registerWithValidData_Ok() throws InvalidUserDataException {
+        System.out.println();
+        User actual = registrationService.register(user);
+        assertEquals(user, actual);
+    }
+
+    @Test
+    void registerWithDuplicateUserLogin_Fail() throws InvalidUserDataException {
+        registrationService.register(user);
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerWithEmptyUserData_Fail() {
+        User user = new User();
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerWithNullUser_Fail() {
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(null));
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void registerUserWithPassword_Fail() {
+        user.setPassword("pass");
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithNullPassword_Fail() {
+        user.setPassword(null);
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithEmptyPassword_Fail() {
+        user.setPassword("");
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithNullLogin_Fail() {
+        user.setLogin(null);
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithEmptyLogin_Fail() {
+        user.setLogin("");
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithLogin_Fail() {
+        user.setLogin("Len");
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUserWithAge_Fail() {
+        user.setAge(17);
+        assertThrows(InvalidUserDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void registerUsersWithValidData_Ok() throws InvalidUserDataException {
+        User user1 = new User();
+        user1.setLogin("user_number1");
+        user1.setAge(27);
+        user1.setPassword("password123");
+        User user2 = new User();
+        user2.setLogin("user_number2");
+        user2.setAge(19);
+        user2.setPassword("password123");
+        User user3 = new User();
+        user3.setLogin("user_number3");
+        user3.setAge(60);
+        user3.setPassword("password123");
+        User actual = registrationService.register(user1);
+        assertEquals(user1, actual);
+        actual = registrationService.register(user2);
+        assertEquals(user2, actual);
+        actual = registrationService.register(user3);
+        assertEquals(user3, actual);
+        assertDoesNotThrow(() -> registrationService.register(user));
+    }
+}
